1d0048e08a6b012b4f5a924d93f12f7829fd6327
emrekibar93 <emrekibar93@gmail.com>
2024-11-14 17:15:56 +0300

    Odev 3 eklendi

---
+++ daireModul/circle.js
@@ -0,0 +1,10 @@
+function circleArea(yaricap){
+    const circlearea=3.14*yaricap*yaricap;
+    console.log(`Daire alan:${circlearea}`)
+}
+
+function circleCircumference(yaricap){
+    const circlecircumreference=2*3.14*yaricap;
+    console.log(`Daire cevresi:${circlecircumreference}`)
+}
+module.exports= {circleArea,circleCircumference}
